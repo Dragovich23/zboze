7c8ffc7856741a5cb89d28825a7371641fad7563
Dragovich23 <69821313+Dragovich23@users.noreply.github.com>
2024-03-24 23:02:19 +0100

    Add files via upload

--- frontend/src/banner/index.js
+++ frontend/src/banner/index.js
@@ -4,9 +4,9 @@ import './Banner.css'; // Import your CSS file for styling
 const Banner = () => {
   const [currentImage, setCurrentImage] = useState(0);
   const images = [
-    'image1.jpg',
-    'image2.jpg',
-    'image3.jpg'
+    './pictures/pszenica.png',
+    './pictures/zyto.png',
+    './pictures/pszenzyto.png'
   ];
 
   const handleClick = () => {
--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import Navbar from './Navbar';
-
-
+import Banner from '../banner';
+import GrainStatisticsComponent from '../statystyki/GrainStatistics';
+import GrainInfo from '../komponentstanowy/component';
 export default function App() {
     return (
         <div>
             <Navbar />
             <div>
-                <h1>Welcome to Our Website</h1>
-                <p>This is a basic template for a front page using React.</p>
+                <h1>Jezu, dajcie mi je. Chcę je w buzi chcę je poczuć. Wielkie nabrzmiałe pszenżyto</h1>
+                <p>Jak kupisz mi traktor to zrobię ci chleb.</p>
             </div>
+            <div>
+            <GrainInfo name="Pszenica" type="Zboże" origin="Podlasie" nutritionalValue="300kcal"/>
+            <GrainStatisticsComponent grainName="Pszenica" grainId="1"/>
+            <GrainStatisticsComponent grainName="Żyto" grainId="2"/>
+            <GrainStatisticsComponent grainName="Pszenżyto" grainId="3"/>
+            </div>
+            <Banner/>
         </div>
     );
 }
